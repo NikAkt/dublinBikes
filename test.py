ccfd0741520f46b87bff83e689eb074434e9e653
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-02-08 10:15:09 +0000

    made code into python

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import requests
 
-def fetch_weather_data(city_name, api_key):
+def fetch_weather_data(city_name, weather_api_key):
     try:
-        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}")
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={weather_api_key}")
         response.raise_for_status()  # Raise an error for bad status codes
         weather_data = response.json()
         return weather_data
@@ -10,14 +10,34 @@ def fetch_weather_data(city_name, api_key):
         print(f"Error fetching weather data: {e}")
         return None
 
-# Example usage
-city_name = 'Dublin'  # Replace with the city name you want to fetch data for
-api_key = '43aeecf5b252d71ca98d7f4dd8aaee24'  # Replace with your OpenWeather API key
+def fetch_bike_data(city_name, bike_api_key):
+    try:
+        response = requests.get(f"https://api.jcdecaux.com/vls/v1/stations?contract={city_name}&apiKey={bike_api_key}")
+        response.raise_for_status()  # Raise an error for bad status codes
+        bike_data = response.json()
+        return bike_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
+        return None
 
-weather_data = fetch_weather_data(city_name, api_key)
+
+city_name = 'Dublin'  # Replace with the city name you want to fetch data for
+weather_api_key = '43aeecf5b252d71ca98d7f4dd8aaee24'  # Replace with your OpenWeather API key
+bike_api_key = '626c8de20316723c1526eed9a83479c9dd13f945'  # Replace with your OpenWeather API key
+
+weather_data = fetch_weather_data(city_name, weather_api_key)
+bike_data=fetch_bike_data(city_name, bike_api_key)
+
 if weather_data:
     # Handle the data
     print(weather_data)
+else:
+    # Handle any errors
+    print("Failed to fetch weather data.")
+
+if bike_data:
+    # Handle the data
+    print(bike_data)
 else:
     # Handle any errors
     print("Failed to fetch weather data.")
