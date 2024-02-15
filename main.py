6baf672b273af43bd0467f7e61a2037db9a70185
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-02-15 10:52:49 +0000

    almost there

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 import datetime
 import time
 import pytz
-
+import pymysql
 
 def fetch_weather_data(city_name, weather_api_key,lat,lon):
     try:
@@ -27,16 +27,32 @@ def fetch_bike_data(city_name, bike_api_key):
         print(f"Error fetching weather data: {e}")
         return None
     
-def fetch_stations(website,bike_api_key,city_name,engine):
+def stations_to_db(text,engine):
     #only needs to run once
-    #Read in stations to json object
-    r = requests.get(website, params={"apiKey": bike_api_key, "contract": city_name})
-    stations = json.loads(r.text)
+    host='se-database.cjm0yeew4eja.eu-north-1.rds.amazonaws.com'
+    user='admin'
+    password='widzEh-kuwriz-0menki'
+    db='dbikes'
+    print("stations_to_db")
+    try:
+        connection = pymysql.connect(host=host,user=user,password=password,db=db)
+        # Execute a simple query to test connectivity
+        with connection.cursor() as cursor:
+            cursor.execute("SHOW VARIABLES;")
+            for row in cursor:
+                print(row)
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
 
-    # Loop through stations, adding each one to the database
+        # Close the connection
+        connection.close()
+
+    stations=json.loads(text)
+    print(type(stations),len(stations))
     for station in stations:
-        values = (station.get("address"),int(station.get("banking")), int(station.get("bike_stands")),station.get("contract_name"), station.get("name"), int.station.get("number"), station.get("position").get("lat"), station.get("position").get("lng"),station.get("status"))
-        engine.execute("INSERT INTO `dbikes`.`station` values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", values)
+        print(station)
+        vals=(station.get('address'),int(station.get('banking')),station.get('bike_stands'),int(station.get('bonus')),station.get('contract_name'),station.get('name'),station.get('number'),station.get('position').get('lat'),station.get('position').get('lng'),station.get('status'))
+        engine.execute("insert into station values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",vals)
     return
 
 
@@ -55,26 +71,34 @@ def main():
     website = "https://api.jcdecaux.com/vls/v1/stations/"
     time = int(datetime.datetime.now().timestamp())
 
-    config = get_db_config()
-    dbuser = config["dbuser"]
-    dbpass = config["dbpass"]
-    dburl = config["dburl"]
-    dbport = config["dbport"]
+    #only needs to run once
+    db_url = 'mysql://admin:widzEh-kuwriz-0menki@se-database.cjm0yeew4eja.eu-north-1.rds.amazonaws.com:3306/dbikes'
+    host='se-database.cjm0yeew4eja.eu-north-1.rds.amazonaws.com'
+    user='admin'
+    password='widzEh-kuwriz-0menki'
+    db='dbikes'
+    port='3306'
 
-    engine = create_engine("mysql+pymysql://{0}:{1}@{2}".format(dbuser, dbpass, dburl), echo=True)
 
-    stations=fetch_stations(website,bike_api_key,city_name,engine)
-    weather_data = fetch_weather_data(city_name, weather_api_key,lat,lon)
-    bike_data=fetch_bike_data(city_name, bike_api_key)
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(user, password, host,port,db), echo=True)
+
+    r = requests.get(website, params={"apiKey": bike_api_key, "contract": city_name})
+    stations_to_db(r.text, engine)
+
+    # weather_data = fetch_weather_data(city_name, weather_api_key,lat,lon)
+    # bike_data=fetch_bike_data(city_name, bike_api_key)
     
-    if weather_data:
-        with open('weather_data.json', 'w') as outfileweather:
-            json.dump(weather_data, outfileweather)
-    else:
-        print("Failed to fetch weather data.")
+    # if weather_data:
+    #     with open('weather_data.json', 'w') as outfileweather:
+    #         json.dump(weather_data, outfileweather)
+    # else:
+    #     print("Failed to fetch weather data.")
 
-    if bike_data:
-        with open('bike_data.json', 'w') as outfilebikes:
-            json.dump(bike_data, outfilebikes)
-    else:
-        print("Failed to fetch bike data.")
+    # if bike_data:
+    #     with open('bike_data.json', 'w') as outfilebikes:
+    #         json.dump(bike_data, outfilebikes)
+    # else:
+    #     print("Failed to fetch bike data.")
+
+if __name__== "__main__":
+    main()
