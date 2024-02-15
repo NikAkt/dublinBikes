df8d12b46d4d691cc093087e3cddaaeaf61334c0
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-02-15 09:47:33 +0000

    sql connection fuckin works

--- test_sql_connection.py
+++ test_sql_connection.py
@@ -1,28 +1,26 @@
 import sqlalchemy
+import pymysql
 
 def test_db_connection():
     # Replace 'your_database_url' with your actual database URL
     db_url = 'mysql://admin:widzEh-kuwriz-0menki@se-database.cjm0yeew4eja.eu-north-1.rds.amazonaws.com:3306/dbikes'
+    host='se-database.cjm0yeew4eja.eu-north-1.rds.amazonaws.com'
+    user='admin'
+    password='widzEh-kuwriz-0menki'
+    db='dbikes'
 
     try:
-        # Establish a connection to the database
-        engine = sqlalchemy.create_engine(db_url)
-        connection = engine.connect()
-
+        connection = pymysql.connect(host=host,user=user,password=password,db=db)
         # Execute a simple query to test connectivity
-        result = connection.execute("SHOW VARIABLES;")
-
-        # Print the results
-        for row in result:
-            print(row)
-
-        print("Database connection successful.")
+        with connection.cursor() as cursor:
+            cursor.execute("SHOW VARIABLES;")
+            for row in cursor:
+                print(row)
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
 
         # Close the connection
         connection.close()
-
-    except Exception as e:
-        print("Error connecting to the database:", e)
 
 # Call the function to test the database connection
 test_db_connection()
