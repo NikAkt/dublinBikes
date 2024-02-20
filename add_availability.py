553a881234f2355ee5a4e278d760ae72612d38a9
Xinnan Tu <lucetu109@gmail.com>
2024-02-20 06:01:57 +0000

    Local test succeeded.

--- add_availability.py
+++ add_availability.py
@@ -48,33 +48,32 @@ def weather_to_db(text, engine):
     """
     Add weather data to the database
     """
-    tz = pytz.timezone('Europe/Dublin')
-    curr = datetime.datetime.now(tz=tz)
+    host = 'se-database.cjm0yeew4eja.eu-north-1.rds.amazonaws.com'
+    user = 'admin'
+    password = 'widzEh-kuwriz-0menki'
+    db = 'dbikes'
+    print("weather_to_db")
+
 
     weather = json.loads(text)
-    vals = (
-        float(weather['coord']['lon']),
-        float(weather['coord']['lat']),
-        int(weather['weather'][0]['id']),
-        float(weather['main']['temp']),
-        float(weather['main']['feels_like']),
-        weather['weather'][0]['description'],
-        float(weather['wind']['speed']),
-        int(weather['main']['humidity']),
-        curr  # Added the current time to the tuple
-    )
-
-    insert_stmt = """
-    INSERT INTO dbikes.weather 
-    (lon, lat, weather_id, temp, feels_like, weather_description, wind_speed, humidity, timestamp) 
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
-
-    with engine.connect() as connection:
-        try:
-            connection.execute(insert_stmt, vals)  # Using the updated vals tuple
-        except Exception as e:
-            print(f"Error executing SQL query: {e}")
+    connection = pymysql.connect(host=host, user=user, password=password, db=db)
+    with connection:
+        with connection.cursor() as cursor:
+            try:
+                vals = (float(weather['coord']['lon']),
+                        float(weather['coord']['lat']),
+                        int(weather['weather'][0]['id']),
+                        float(weather['main']['temp']),
+                        float(weather['main']['feels_like']),
+                        weather['weather'][0]['description'],
+                        float(weather['wind']['speed']),
+                        int(weather['main']['humidity']))
+                cursor.execute("INSERT INTO `dbikes`.`weather` values(%s,%s,%s,%s,%s,%s,%s,%s)", vals)
+            except Exception as e:
+                print(f"Error executing SQL query: {e}")
+                connection.rollback()
+        connection.commit()
+    return
 
 
 def main():
