0229425ff1655194054cc4ec489b5fb202d869b4
Ellen Doherty <ellenmdoherty@gmail.com>
2024-02-25 17:25:56 +0000

    GMAPS MARKERs
    Google maps pinpoints, info windows for each marker, dropdown menus populated

--- static/test2.js
+++ static/test2.js
@@ -1,14 +1,62 @@
+let bikeStations = [];
+let infoWindow;
 
+// fetching the data from the api
+async function fetchDublinBikesData() {
+  try {
+      const response = await fetch('https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=626c8de20316723c1526eed9a83479c9dd13f945');
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error('Error fetching Dublin Bikes data:', error);
+      return null;
+  }
+}
+//creating the markers using bike data
+function createMarkers(map, bikeStations) {
+  bikeStations.forEach(station => {
+    const marker = new google.maps.Marker({
+      position: station.position,
+      map: map,
+      title: station.name,  
+    });
+    marker.addListener('click', () => {
+      if (infoWindow) {
+          infoWindow.close(); 
+      }
+      infoWindow = new google.maps.InfoWindow({
+          content: `<div>${station.name}<br>Available Bikes: ${station.available_bikes}</div>`
+      });
+      infoWindow.open(map, marker);
+  });
+
+  });
+  
+}
+//function to update the markers every 5 mins
+async function updateMarkers() {
+  const updatedData = await fetchDublinBikesData();
+
+  if (updatedData !== null) {
+      bikeStations = updatedData;
+      createMarkers(map, bikeStations);
+      
+  } else {
+      console.error('Data not found.');
+  }
+}
+
+// calling the init map function for displaying google map//
 document.addEventListener('DOMContentLoaded', function () {
   initMap();
 });
-
+//the init map function for displaying google map
 async function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8
   });
-
+//setting the default display location of the map
   const mapDiv = document.getElementById('map');
   const mapCenter = { lat: 53.3483031, lng: -6.2637067 };
 
@@ -20,10 +68,19 @@ async function initMap() {
               zoom: 13
           });
       }
+      bikeStations = await fetchDublinBikesData();
+      
+// Create markers using the data
+        createMarkers(map, bikeStations);
+
+//populateDropdown
+        populateDropdown('#startSelector select', bikeStations);
+        populateDropdown('#destinationSelector select', bikeStations);
+        
+
   } catch (error) {
       console.error('Error importing marker library:', error);
   }
-
 
 
 
@@ -43,7 +100,7 @@ if (weatherButton){
       })
     }
 
-//get todays date
+//get todays date and time
 function getCurrentDateTime() {
   const today = new Date();
   const year = today.getFullYear();
@@ -58,3 +115,17 @@ function getCurrentDateTime() {
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('dateTimeInput').value = getCurrentDateTime();
 })}
+
+
+//function to populate the dropwdowns with the stations
+function populateDropdown(selector, stations) {
+  const selectElement = document.querySelector(selector);
+  selectElement.innerHTML = `<option value="">Select a Station</option>`;
+
+  stations.forEach(station => {
+    const option = document.createElement('option');
+    option.value = `station${station.number}`;
+    option.textContent = station.name;
+    selectElement.appendChild(option);
+  });
+}
