be130902ec011086045d8bcb251c3cc5118b8281
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-02-27 17:38:40 +0000

    availability route for flask is done

--- app.py
+++ app.py
@@ -35,6 +35,27 @@ def get_stations():
                 print(row) #test print
                 stations.append(dict(row))
             return jsonify(station=stations)
+        
+@app.route("/availability")
+def get_availability():
+    """
+    Get availability
+    render template to client
+    """
+    host = 'se-database.cjm0yeew4eja.eu-north-1.rds.amazonaws.com'
+    user = 'admin'
+    password = 'widzEh-kuwriz-0menki'
+    db = 'dbikes'
+    availability=[]
+    connection = pymysql.connect(host=host, user=user, password=password, db=db,cursorclass=DictCursor)
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT * FROM dbikes.availability ORDER BY curr_time DESC LIMIT 114;""")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row) #test print
+                availability.append(dict(row))
+            return jsonify(availability=availability)
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
