fe3edd100d14365be28425d18b323f7f32f86aba
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-03-05 14:35:49 +0000

    the journey planner now shows the current date and time

--- static/test2.js
+++ static/test2.js
@@ -184,11 +184,12 @@ function getCurrentDateTime() {
   const day = String(today.getDate()).padStart(2, '0');
   const hours = String(today.getHours()).padStart(2, '0');
   const minutes = String(today.getMinutes()).padStart(2, '0');
+  const seconds= String(today.getSeconds()).padStart(2, '0');
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('dateTimeInput').value = getCurrentDateTime();
+  document.getElementById('dateInput').value = getCurrentDateTime();
 })
 
 //function to populate the dropwdowns with the stations
