a2a6544a6ead43a82c2f5afb7f91972a341c0e4b
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-04-05 18:19:28 +0100

    Close to completing the API
    -flask has can access the models now
    -js sends date, time and station numbers to flask
    -get future weather forecast and put it into weather_data.json

--- app.py
+++ app.py
@@ -1,15 +1,73 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import pymysql
+import json
 from pymysql.cursors import DictCursor
+import joblib
+import requests
 from datetime import datetime, timedelta
+from dateutil.parser import parse
 
 station_count=0
 
 load_dotenv()
 app = Flask("__name__", template_folder='templates', static_folder='static')
+CORS(app)
 
+models_availability={}
+models_bike_stands={}
+models_availability_directory="models_availability"
+models_bike_stands_directory="models_bike_stands"
+
+for filename in os.listdir(models_availability_directory):
+    if filename.endswith('.joblib'):
+        # Get the station number from the filename
+        station_number = int(filename.split('model_availability')[1].split('.')[0])
+        # Load the model and store it in the dictionary
+        models_availability[station_number] = joblib.load(os.path.join(models_availability_directory, filename))
+
+for filename in os.listdir(models_bike_stands_directory):
+    if filename.endswith('.joblib'):
+        # Get the station number from the filename
+        station_number = int(filename.split('model_bike_stand')[1].split('.')[0])
+        # Load the model and store it in the dictionary
+        models_bike_stands[station_number] = joblib.load(os.path.join(models_bike_stands_directory, filename))
+
+def get_weather_forecast():
+    """
+    Accesses the openweathermap API to get the weather forecast for the next 5 days.
+    """
+    website_weather = "https://api.openweathermap.org/data/2.5/forecast?"
+    lat='53.2734' #Dublin's latitude
+    lon='-7.77832031' #Dublin's longitude
+    weather_api_key = '43aeecf5b252d71ca98d7f4dd8aaee24'
+    weather = requests.get(website_weather, params={"lat": lat, "lon": lon,"appid":weather_api_key })
+    forecast = weather.json()
+
+    with open('weather_forecast.json','w') as f:
+        json.dump(forecast, f)
+    return
+
+def process_weather_forecast(weather_json, dateTime):
+    """
+    Processes the weather forecast JSON to extract the relevant weather data for the given timestamp.
+    """
+    print(weather_json)
+    print("=====================================")
+    for item in weather_json['list']:
+        if item['dt_txt'] == dateTime:
+            temp_k = item['main']['temp']
+            feels_like_k = item['main']['feels_like']
+            wind_speed = item['wind']['speed']
+            humidity = item['main']['humidity']
+
+            temp_c = temp_k - 273.15
+            feels_like_c = feels_like_k - 273.15
+
+            return temp_c, feels_like_c, wind_speed, humidity
+    return None, None, None, None
 
 @app.route("/")
 def index():
@@ -38,14 +96,10 @@ def get_stations():
             result = cursor.fetchone()
             count_list = list(result.values())
             station_count = int(count_list[0])
-            print("==============================================")
-            print("Station Count: ",station_count)
-            print("==============================================")
 
             cursor.execute("SELECT * FROM station;")
             rows = cursor.fetchall()
             for row in rows:
-                # print(row) #test print
                 stations.append(dict(row))
             return jsonify(station=stations)
         
@@ -64,17 +118,11 @@ def get_availability():
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(f"SELECT * FROM dbikes.availability ORDER BY curr_time DESC LIMIT {station_count};")
-            print(station_count)
             rows = cursor.fetchall()
             row_count=0
             for row in rows:
-                print(row) #test print
                 availability.append(dict(row))
                 row_count+=1
-            print("==============================================")
-            print("Row Count: ",row_count)
-            print("==============================================")
-
             return jsonify(availability=availability)
         
 @app.route("/availability_by_hour/<int:station_id>")
@@ -120,6 +168,67 @@ def get_availability_by_hour(station_id):
 
     return jsonify(availability=availability)
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    model_inputs = request.get_json()
+    get_weather_forecast()
+    # proc_weather_data=process_weather_forecast(weather, data['timestamp'])
+    
+    # hour = (timestamp.hour // 3) * 3
+    # timestamp = timestamp.replace(hour=hour, minute=0, second=0)
+    # timestamp = int(timestamp.timestamp())
+
+    temp_datetime = (model_inputs['timestamp'])
+
+    round_times = [0, 3, 6, 9, 12, 15, 18, 21]
+    hour = temp_datetime.hour
+    next_hour = min((t for t in round_times if t >= hour), default=0)
+    if next_hour == 0:
+        temp_datetime += timedelta(days=1)
+
+    # Replace the hour of temp_datetime with next_hour
+    temp_datetime = temp_datetime.replace(hour=next_hour, minute=0, second=0, microsecond=0)
+
+    with open('processed_weather_data.json', 'r') as f:
+        weather = json.load(f)
+
+    temp_c, feels_like_c, wind_speed, humidity = process_weather_forecast(weather, temp_datetime)
+
+    print(temp_c, feels_like_c, wind_speed, humidity)
+
+    station_number_start = int(data['station_number_start'])
+    station_number_end = int(data['station_number_end'])
+
+    year=data['year']
+    month=data['month']
+    day=data['day']
+    hour=data['hour']
+    minute=data['minute']
+    day_of_week=data['day_of_week']
+
+    # temp=data['temp']
+    # feels_like=data['feels_like']
+    # wind_speed=data['wind_speed']
+    # humidity=data['humidity']
+
+    # Get model for this station
+
+    model_av = models_availability[station_number_start]
+    model_stand=models_bike_stands[station_number_end]
+    # print(model_av)
+    # print(model_stand)
+
+    input_data = [[temp_c,feels_like_c, wind_speed,humidity,year, month, day, hour, minute, day_of_week]]
+    input_data = [[year, month, day, hour, minute, day_of_week]]
+
+    prediction_availability = model_av.predict(input_data)
+    prediction_bike_stands = model_stand.predict(input_data)
+
+    return {
+        'prediction_bike_availability_start': prediction_availability.tolist(),
+
+        'prediction_bike_stands_end': prediction_bike_stands.tolist()
+    }
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
     
